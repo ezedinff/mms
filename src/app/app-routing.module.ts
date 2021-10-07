b47ea1ecdea7bd01375e143eacf188e602e42609
hundesoft <hunderegesa@gmail.com>
2021-10-07 08:37:38 +0300

    login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
 import { LoginComponent } from './auth/login/login.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -5,6 +6,7 @@ import { Router, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes=[
 {path:"login",component:LoginComponent},
+{path:"recoverpassword", component:ForgotPasswordComponent},
 {path:"", component:LoginComponent, pathMatch:"full"}
 ];
 
