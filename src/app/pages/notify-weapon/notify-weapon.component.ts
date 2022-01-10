7b17dba46b6d3d3a00e0db777ea21e004b41b70f
hundesoft <hunderegesa@gmail.com>
2022-01-10 16:44:56 +0300

    modification

--- src/app/pages/notify-weapon/notify-weapon.component.ts
+++ src/app/pages/notify-weapon/notify-weapon.component.ts
@@ -13,7 +13,7 @@ export class NotifyWeaponComponent implements OnInit {
   form = NotifyWeaponForm;
   url = "http://localhost:3000/notifies";
   
-  excludeColumns = ['id'];
+  excludeColumns = ['description'];
 
 
   actions: Array<Action> = [
